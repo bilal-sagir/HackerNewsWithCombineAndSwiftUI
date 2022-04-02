3a3dd3cb5924bf882b9def3044c71dd839368bff
bilal-sagir <sagir.bilal@icloud.com>
2022-04-02 23:57:21 +0300

    add get story by Id

---
+++ HackerNews/Models/Story.swift
@@ -0,0 +1,15 @@
+//
+//  Story.swift
+//  HackerNews
+//
+//  Created by Bilal on 2.04.2022.
+//
+
+import Foundation
+
+struct Story: Codable {
+    let id: Int
+    let title: String
+    let url: String
+    
+}
--- HackerNews/Webservice.swift
+++ HackerNews/Webservice.swift
@@ -9,6 +9,19 @@ import Foundation
 import Combine
 
 class Webservice{
+    
+    func getStoryById(storyId: Int) -> AnyPublisher<Story, Error> {
+        
+        guard let url = URL(string: "https://hacker-news.firebaseio.com/v0/item/\(storyId).json?print=pretty") else { fatalError("Invalid URL") }
+        
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .receive(on: RunLoop.main)
+            .map(\.data)
+            .decode(type: Story.self, decoder: JSONDecoder())
+            .catch { _ in Empty<Story, Error>() }
+            .eraseToAnyPublisher()
+    }
+    
     func getAllTopStories() -> AnyPublisher<[Int], Error> {
         
         guard let url = URL(string: "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty") else {
